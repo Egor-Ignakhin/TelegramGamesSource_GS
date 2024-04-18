97e6d95ad2f18158b9ee52c45dbcd83724ea7ccb
Kurochkin Egor <willson2019.octa@mail.ru>
2024-04-18 18:09:21 +0400

    upd

--- handlers/keyboards.py
+++ handlers/keyboards.py
@@ -7,7 +7,7 @@ from aiogram.types.web_app_info import WebAppInfo
 def web_app_keyboard() -> ReplyKeyboardMarkup:
     buttons = [
         [
-            KeyboardButton(text='Все игры 🎲', web_app=WebAppInfo(url="https://kurochki-n.github.io/PortfolioPage/")),
+            KeyboardButton(text='Все игры 🎲', web_app=WebAppInfo(url="https://kurochki-n.github.io/Portfolio-Page/")),
         ]
     ]
     keyboard = ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
